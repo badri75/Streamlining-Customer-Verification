9d5d95852f52845fd7ea9c557bec38718c872f4c
Badri Dell Vostro 3480 <rbadri7501@gmail.com>
2023-04-01 22:08:50 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streamlining_Customer_Verification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
